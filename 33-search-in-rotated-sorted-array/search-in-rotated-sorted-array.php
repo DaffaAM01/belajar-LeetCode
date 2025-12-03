40546d15a63ce61fc39fafafcd12b8719664a9bd
DaffaAM01 <dafmer809@gmail.com>
2025-12-03 09:26:30 +0700

    Time: 0 ms (100.00%) | Memory: 20.8 MB (22.58%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.php
@@ -0,0 +1,36 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    function search($nums, $target) {
+        $left = 0;
+        $right = count($nums) - 1;
+
+        while ($left <= $right) {
+            $mid = floor(($left + $right) / 2);
+
+            if ($nums[$mid] === $target) {
+                return $mid;
+            }
+
+            if ($nums[$left] <= $nums[$mid]) {
+                if ($target >= $nums[$left] && $target < $nums[$mid]) {
+                    $right = $mid - 1;
+                } else {
+                    $left = $mid + 1;
+                }
+            } else {
+                if ($target > $nums[$mid] && $target <= $nums[$right]) {
+                    $left = $mid + 1;
+                } else {
+                    $right = $mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
