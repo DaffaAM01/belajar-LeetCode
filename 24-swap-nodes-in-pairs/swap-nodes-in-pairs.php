18da1eafbfc3b39fe62c1c67f7a144a64ed9ff89
DaffaAM01 <dafmer809@gmail.com>
2025-12-03 09:02:34 +0700

    Time: 0 ms (100.00%) | Memory: 20.3 MB (100.00%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.php
@@ -0,0 +1,35 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function swapPairs($head) {
+        $dummy = new ListNode(0, $head);
+        $current = $dummy;
+
+        while ($current->next !== null && $current->next->next !== null) {
+            $first = $current->next;
+            $second = $current->next->next;
+
+            $current->next = $second;
+            $first->next = $second->next;
+            $second->next = $first;
+
+            $current = $first;
+        }
+
+        return $dummy->next;
+    }
+}
